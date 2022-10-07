fbad7403a3f3b7044d65c3d61158bbef3eae61be
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-10-07 16:12:13 +0200

    fixing slider index bug

--- dist/js/gallery.js
+++ dist/js/gallery.js
@@ -1,51 +1,62 @@
-const gallery = document.querySelector(".gallery")
-const galleryModal = document.querySelector(".gallery-modal")
+const gallery = document.querySelector(".gallery");
+const galleryModal = document.querySelector(".gallery-modal");
 const modalImage = document.querySelector(".gallery-modal__img");
+const modalDecription = document.querySelector(".gallery-modal__decription");
 const images = document.querySelectorAll(".gallery__link");
 const nextBtn = document.querySelector(".gallery-modal__next");
 const prevBtn = document.querySelector(".gallery-modal__prev");
 
-arrayOfImg = Array.from(images)
-let counter = 0;
-console.log(arrayOfImg)
+let arrayOfImg = Array.from(images); //strożenie tablicy z NodeList zdjęć
+let counter = 0; //wartość początkowa licznika do przycisków next i prev
+let src;
+let alt
 
-const openGalleryModal = (e) =>{
-    if(e.target.matches(".gallery__link")){
-        let src = e.target.getAttribute("src");
-        let alt = e. target.getAttribute("alt")
-        document.querySelector(".gallery-modal__img").src = src;
-        document.querySelector(".gallery-modal__decription").textContent = alt
-        galleryModal.classList.add("open")
-        galleryModal.style.display = "block"
-        document.querySelector("body").classList.add("stopScroll")
+const openGalleryModal = (e) => {
+  if (e.target.matches(".gallery__link")) {
+    src = e.target.getAttribute("src"); //pobranie atrybutu obrazu
+    alt = e.target.getAttribute("alt"); //pobranie atrybutu opisu
+    modalImage.src = src;   //nadanie modalowi atrubutu obrazu
+    modalDecription.textContent = alt; //nadanie opisowi atrtybutu alt
+    galleryModal.classList.add("open");
+    galleryModal.style.display = "block";
+    document.querySelector("body").classList.add("stopScroll");
+  }
+  arrayOfImg.forEach((item, index) => { //sprawdzenie jeżeli kliknięty obraz jest równy obrazowi z tablicy
+    if (modalImage.src === item.src) {  //to wtedy couter sotaje watość indeksu elementu z tablycy
+      return (counter = index);         //żeby przy klinięciu następny couter nie liczył od 0
     }
-}
+  });
+};
 
-const closeGalleryModal = e =>{
-    if(e.target.matches(".gallery-modal")){
-        galleryModal.style.display = "none";
-        galleryModal.classList.remove("open")
-        document.querySelector("body").classList.remove("stopScroll")
-    }
-}
+const closeGalleryModal = (e) => {
+  if (e.target.matches(".gallery-modal")) {
+    galleryModal.style.display = "none";
+    galleryModal.classList.remove("open");
+    modalImage.removeAttribute("src");
+    counter = 0;
+    document.querySelector("body").classList.remove("stopScroll");
+  }
+};
 
-const nextSlide = () =>{
-    if(counter >= arrayOfImg.length -1) counter = -1
-    counter++;
-    return setImg();
-}
+const nextSlide = () => {
+  if (counter >= arrayOfImg.length - 1) counter = -1; //sprawdzenie czy wartość coutera jest równa 
+  counter++;                                           //długości tablicy, jeżeli tak to resetuj couter 
+  return setImg();
+};
 
-const prevSlide = () =>{
-    if(counter <=0) counter = arrayOfImg.length;
-    counter--
-    return setImg();
-}
+const prevSlide = () => {
+  if (counter <= 0) counter = arrayOfImg.length; //sprawdzenie czy couter jest = 0, jeżeli tak to ustaw 
+  counter--;                                    //counter na długośc tablicy
+  return setImg();
+};
 
-const setImg = () =>{
-   return modalImage.setAttribute("src", arrayOfImg[counter].src)
-}
+const setImg = () => {
+  modalImage.setAttribute("src", arrayOfImg[counter].src);
+  modalDecription.textContent = arrayOfImg[counter].alt
+};
 
-gallery.addEventListener("click", openGalleryModal)
-galleryModal.addEventListener("click", closeGalleryModal)
-nextBtn.addEventListener("click", nextSlide)
-prevBtn.addEventListener("click", prevSlide)
+gallery.addEventListener("click", openGalleryModal);
+galleryModal.addEventListener("click", closeGalleryModal);
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener("click", prevSlide);
+
