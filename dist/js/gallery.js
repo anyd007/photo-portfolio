92cfe8884cebf3198666c254dc904bb9253e4e60
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-10-07 11:27:12 +0200

    create slider arrows with javascript

--- dist/js/gallery.js
+++ dist/js/gallery.js
@@ -1,5 +1,13 @@
 const gallery = document.querySelector(".gallery")
 const galleryModal = document.querySelector(".gallery-modal")
+const modalImage = document.querySelector(".gallery-modal__img");
+const images = document.querySelectorAll(".gallery__link");
+const nextBtn = document.querySelector(".gallery-modal__next");
+const prevBtn = document.querySelector(".gallery-modal__prev");
+
+arrayOfImg = Array.from(images)
+let counter = 0;
+console.log(arrayOfImg)
 
 const openGalleryModal = (e) =>{
     if(e.target.matches(".gallery__link")){
@@ -7,6 +15,7 @@ const openGalleryModal = (e) =>{
         let alt = e. target.getAttribute("alt")
         document.querySelector(".gallery-modal__img").src = src;
         document.querySelector(".gallery-modal__decription").textContent = alt
+        galleryModal.classList.add("open")
         galleryModal.style.display = "block"
         document.querySelector("body").classList.add("stopScroll")
     }
@@ -15,9 +24,28 @@ const openGalleryModal = (e) =>{
 const closeGalleryModal = e =>{
     if(e.target.matches(".gallery-modal")){
         galleryModal.style.display = "none";
+        galleryModal.classList.remove("open")
         document.querySelector("body").classList.remove("stopScroll")
     }
 }
 
+const nextSlide = () =>{
+    if(counter >= arrayOfImg.length -1) counter = -1
+    counter++;
+    return setImg();
+}
+
+const prevSlide = () =>{
+    if(counter <=0) counter = arrayOfImg.length;
+    counter--
+    return setImg();
+}
+
+const setImg = () =>{
+   return modalImage.setAttribute("src", arrayOfImg[counter].src)
+}
+
 gallery.addEventListener("click", openGalleryModal)
 galleryModal.addEventListener("click", closeGalleryModal)
+nextBtn.addEventListener("click", nextSlide)
+prevBtn.addEventListener("click", prevSlide)
