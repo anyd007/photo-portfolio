2fa61991dfaffcde754e0cb8e033d3e519f96e2c
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-10-10 14:57:06 +0200

    added lazzy loading in js

--- dist/js/gallery.js
+++ dist/js/gallery.js
@@ -3,6 +3,7 @@ const galleryModal = document.querySelector(".gallery-modal");
 const modalImage = document.querySelector(".gallery-modal__img");
 const modalDecription = document.querySelector(".gallery-modal__decription");
 const images = document.querySelectorAll(".gallery__link");
+const galleryItems = document.querySelectorAll(".gallery__item");
 const nextBtn = document.querySelector(".gallery-modal__next");
 const prevBtn = document.querySelector(".gallery-modal__prev");
 
@@ -40,13 +41,16 @@ const closeGalleryModal = (e) => {
   }
 };
 
+//button następny
 const nextSlide = () => {
   if (counter >= arrayOfImg.length - 1) counter = -1; //sprawdzenie czy wartość coutera jest równa 
   counter++;                                           //długości tablicy, jeżeli tak to resetuj couter 
   return setImg();
 };
 
+//button poprzedni
 const prevSlide = () => {
+  console.log(modalImage)
   if (counter <= 0) counter = arrayOfImg.length; //sprawdzenie czy couter jest = 0, jeżeli tak to ustaw 
   counter--;                                    //counter na długośc tablicy
   return setImg();
@@ -56,9 +60,26 @@ const setImg = () => {
   modalImage.setAttribute("src", arrayOfImg[counter].src);
   modalDecription.textContent = arrayOfImg[counter].alt
   modalImage.classList.toggle("open")
-
-
+  
 };
+
+//efekt pojawiania się zdjęć w galeri
+const options ={
+  threshold: 1,
+  rootMargin: "0px 0px 200px 0px",
+ }
+const apperOnScroll = new IntersectionObserver((entries, apperOnScroll)=>{
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          return
+        }
+        entry.target.classList.add("open");
+        apperOnScroll.unobserve(entry.target);
+      })
+}, options);
+galleryItems.forEach((galleryItem) => {
+  apperOnScroll.observe(galleryItem);
+});
 
 gallery.addEventListener("click", openGalleryModal);
 galleryModal.addEventListener("click", closeGalleryModal);
--- dist/js/modal.js
+++ dist/js/modal.js
@@ -1,5 +1,6 @@
 const modal = document.querySelector(".modal");
 const media = document.querySelector(".media")
+const images = document.querySelectorAll("[data-src]")
 
 const openModal = e =>{
     if(e.target.matches(".media__item--img")){
@@ -16,6 +17,36 @@ const closeModal = e =>{
         document.querySelector("body").classList.remove("stopScroll")
     }
 }
+
+//lazy loading
+const preLoadImage = (img) =>{
+    const src = img.getAttribute("data-src")
+    if(!src){
+        return
+    }
+    img.src = src
+}
+const options = {
+    root: null,
+    //zakres od 0 do 1, jeżeli 0 to odpali się "as soon as possible", jeżeli 1 to
+    //musi być w 100% we viewpoint
+    threshold: 0,       
+    //usawaimy margin kiedy ma odpalić obeseratora
+    rootMargin: "0px 0px 100px 0px",
+}
+const imgObserver = new IntersectionObserver((entries, imgObserver)=>{
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){
+            return
+        }
+        preLoadImage(entry.target)
+        imgObserver.unobserve(entry.target)
+    })
+}, options)
+
+images.forEach(image=>{
+    imgObserver.observe(image)
+})
 
 media.addEventListener("click", openModal)
 modal.addEventListener("click", closeModal)
